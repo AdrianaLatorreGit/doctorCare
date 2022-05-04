f8a8fdb35e5fd29e9a31fced83887c8f5bf4200c
AdrianaLatorreGit <dri.latorre@gmail.com>
2022-05-04 14:05:06 -0300

    more html css js

--- main.js
+++ main.js
@@ -6,3 +6,13 @@ function onScroll() {
         navigation.classList.remove("scroll");
     }
 }
+
+// Ativa botão hamburguer on click (botão openMenu)
+function openMenu() {
+    document.body.classList.add("menu-expended");
+}
+
+// clicar no botão de fechar, dentro do expanded e sumir aquela navegação (botão closeMenu)
+function closeMenu() {
+    document.body.classList.remove("menu-expended");
+}
